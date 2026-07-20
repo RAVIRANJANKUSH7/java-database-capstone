4b4ab5ccaeec7d895dffdcca37bee6351eaa0c9c
RAVIRANJANKUSH7 <rrluvkush420@gmail.com>
2026-07-20 20:02:04 +0530

    Create Doctor.java

---
+++ Doctor.java
@@ -0,0 +1,92 @@
+package com.project.back_end.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "Doctor")
+public class Doctor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "doctor_id")
+    private Long doctorId;
+
+    @NotBlank
+    private String name;
+
+    @Email
+    @NotBlank
+    @Column(unique = true)
+    private String email;
+
+    @NotBlank
+    private String password;
+
+    @NotBlank
+    private String speciality;
+
+    @Column(name = "available_time")
+    private String availableTime;
+
+    public Doctor() {
+    }
+
+    public Doctor(String name, String email, String password,
+                  String speciality, String availableTime) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.speciality = speciality;
+        this.availableTime = availableTime;
+    }
+
+    public Long getDoctorId() {
+        return doctorId;
+    }
+
+    public void setDoctorId(Long doctorId) {
+        this.doctorId = doctorId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSpeciality() {
+        return speciality;
+    }
+
+    public void setSpeciality(String speciality) {
+        this.speciality = speciality;
+    }
+
+    public String getAvailableTime() {
+        return availableTime;
+    }
+
+    public void setAvailableTime(String availableTime) {
+        this.availableTime = availableTime;
+    }
+}
