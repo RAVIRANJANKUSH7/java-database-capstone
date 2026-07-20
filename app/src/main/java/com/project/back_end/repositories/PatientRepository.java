31ba7eb0e6397ccf317da27fdda715181c12b64f
RAVIRANJANKUSH7 <rrluvkush420@gmail.com>
2026-07-20 21:43:06 +0530

    Update PatientRepository.java

--- app/src/main/java/com/project/back_end/repositories/PatientRepository.java
+++ app/src/main/java/com/project/back_end/repositories/PatientRepository.java
@@ -4,12 +4,10 @@ import com.project.back_end.models.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface PatientRepository extends JpaRepository<Patient, Long> {
 
-    Optional<Patient> findByEmail(String email);
+    Patient findByEmail(String email);
 
-    boolean existsByEmail(String email);
+    Patient findByEmailOrPhone(String email, String phone);
 }
